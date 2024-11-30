7bb9eb947c9edbe54d06a3ccd48d193fdbc6157a
Your New Username <danieokandeji@gmail.com>
2024-11-30 20:21:29 +0100

    first

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const socketIo = require("socket.io");
-const { sequelize } = require("./models");
+const { sequelize } = require("./Models");
 require("dotenv").config();
 
 const app = express();
